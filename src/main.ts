4f32e372431f86226cbbe4e39b0050a1ca5583fc
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-12-17 11:41:27 -0400

    Criação de uma classe de banco de dados específica

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
 import cors from 'cors'
+import BancoMysql from './db/banco-mysql'
 
 const app = express()
 app.use(express.json())
@@ -9,15 +10,10 @@ app.use(cors())
 
 app.get("/produtos", async (req, res) => {
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022a",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from produtos")
-        await connection.end()
+        const banco = new BancoMysql()
+        await banco.criarConexao()
+        const result = await banco.consultar()
+        await banco.finalizarConexao()
         res.send(result)
     } catch (e) {
         console.log(e)
