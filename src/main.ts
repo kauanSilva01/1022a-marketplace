c0b346945638022d3d0e36831db980e1f5d6901d
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-13 09:54:02 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,22 +34,23 @@ app.post("/produtos", async (req, res) => {
     }
 })
 
+//DELETAR
+app.delete("/produtos/:id",async(req,res)=>{
+    console.log(req.params.id)
+    const query = "DELETE FROM produtos WHERE id = ?"
+    const parametros = [req.params.id]
 
-app.get("/usuarios", async (req, res) => {
-    try {
-        const connection = await mysql.createConnection({
-            host: process.env.dbhost ? process.env.dbhost : "localhost",
-            user: process.env.dbuser ? process.env.dbuser : "root",
-            password: process.env.dbpassword ? process.env.dbpassword : "",
-            database: process.env.dbname ? process.env.dbname : "banco1022a",
-            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
-        })
-        const [result, fields] = await connection.query("SELECT * from usuarios")
-        await connection.end()
-        res.send(result)
-    } catch (e) {
-        res.status(500).send("Server ERROR")
-    }
+    const banco = new BancoMysql()
+    await banco.criarConexao()
+    const result = await banco.consultar(query,parametros)
+    await banco.finalizarConexao()
+    res.send("Produto excluido com sucesso id: "+req.params.id)
+})
+
+//ALTERAR
+app.put("/produtos/:id",(req,res)=>{
+    console.log(req.params.id)
+    
 })
 
 app.listen(8000, () => {
