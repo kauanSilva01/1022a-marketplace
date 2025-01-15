1f075178b3adf4f7dc75b16c6d9afe9356915779
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-01-15 11:37:45 -0400

    Update banco-mongo.ts

--- src/db/banco-mongo.ts
+++ src/db/banco-mongo.ts
@@ -10,6 +10,32 @@ class BancoMongo{
         if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
         await this.connection.close()
     }
+    async listar(){
+        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+        const db = this.connection.db("banco1022a");
+        const result = db.collection('produtos').find().toArray()
+        return result
+    }
+    async inserir(produto:{id:string,nome:string,descricao:string,preco:string,imagem:string}){
+        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+        const db = this.connection.db("banco1022a");
+        const result = db.collection('produtos').insertOne(produto)
+        return result
+    }
+    async excluir(id:string){
+        const idNumber = parseInt(id)
+        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+        const db = this.connection.db("banco1022a");
+        const result = db.collection('produtos').deleteOne({idNumber})
+        return result
+    }
+    async alterar(id:string,produto:{id?:string,nome:string,descricao:string,preco:string,imagem:string}){
+        const idNumber = parseInt(id)
+        if(!this.connection) throw new Error("Erro de conexão com o banco de dados.")
+        const db = this.connection.db("banco1022a");
+        const result = db.collection('produtos').updateOne({idNumber},{$set:produto})
+        return result
+    }
 }
 
 export default BancoMongo
